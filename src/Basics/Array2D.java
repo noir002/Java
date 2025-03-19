936a71754fd0d29dd873935efe937c02a3d4820e
noir@002 <paras.chauhan.93893@gmail.com>
2025-03-19 18:12:51 +0530

    Day 15 java

---
+++ src/Basics/Array2D.java
@@ -0,0 +1,4 @@
+package Basics;
+
+public class Array2D {
+}
---
+++ src/Basics/ArraysBasics.java
@@ -0,0 +1,24 @@
+package Basics;
+import java.util.*;
+
+public class ArraysBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //int n = sc.nextInt();
+        //int arr[]= new int[n];
+        int arr[] = {4,5,6,7};
+
+        //for(int j = 0; j<n; j++){
+
+        //}
+
+        for(int i = 0; i < 4 ; i++){
+            System.out.println(arr[i]);
+        }
+
+
+
+
+    }
+}
